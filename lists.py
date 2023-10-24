270155f94ece0ba7573393d113675b38914d5956
Fred Denis <fredjamo68@gmail.com>
2023-10-24 12:52:53 +0300

    initial commit

---
+++ lists.py
@@ -0,0 +1,60 @@
+numbers = [1, 3,4,5]
+print(numbers)
+print(len(numbers))
+# Looping over lists
+for number in numbers:
+    if number == 5:
+       print("Found number", number)
+    else:
+        print(number)
+
+list_nested = [[1, 2, 2, 4], ["cat", "bird", "bat"]]
+print(list_nested[0][1])
+print(list_nested[1][0])
+
+# Negative indexes
+span = ["cat", "bird", "bat"]
+print(span[-1] + span[-2])
+
+# Slicing lists
+sliced_list = span[0:2]
+print(sliced_list)
+
+# Living out some indices 
+print(span[:2])
+print(span[0:-1])
+
+# List Concatenation and List Replication
+
+# Removing Values from list using del
+groups = ["cat", "bird", "bat"]
+del groups[1]
+print("Groups:", groups)
+
+# Range
+for i in range(6):
+    print(i)
+
+# Using loops with lists
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+for item in range(len(supplies)):
+    print("Index " + str(item) + " in  supplies is " + supplies[item])
+
+# Using random.choice() and random.shuffle() functions with lists
+
+import random
+pets = ["cat", "Dog", "Rackoon", "Fish"]
+choose = random.choice(pets)
+print("Random choice", choose)
+
+# Shuffle lists
+
+people = ["Bobi", "Alexa", "Ali", "Siri", "Kabu"]
+shuffled = random.shuffle(people)
+print("Shuffled list", shuffled)
+
+span = 42
+span += 3
+print(span, "")
+
+# Finding a vlue in  a list using index() method
---
+++ variables.py
@@ -0,0 +1,26 @@
+first_name = "Dan"
+second_name = "Wan"
+
+print(first_name)
+# This program says hello and asks for my name.
+# print("Hello there")
+# print("What is your name?")
+# first_name = input("Enter your name: ")
+# print("It is nice to meet you ", first_name)
+# print("Your names length is ", len(first_name))
+# print("What is your age?")
+# myAge = input("Enter your age ");
+# print("Your age is ", myAge);
+
+# Converting variables
+print(42 == "42")
+name = 45
+name_string = str(name)
+print("Coder" + name_string )
+# Binary Boolean Operators
+print(not True)
+print(True and True)
+print(True and False)
+
+
+
